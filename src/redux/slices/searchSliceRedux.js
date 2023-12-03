ce7904b1c08b95b8d06fe23b541552817500e60d
DinaX <dinisantunes@ua.pt>
2023-12-03 23:05:35 +0000

    comentarios adicionados

--- src/redux/slices/searchSliceRedux.js
+++ src/redux/slices/searchSliceRedux.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-
+//action assincrona da pesquisa por livros
 export const searchBooks = createAsyncThunk('search/searchBooks', async (searchBook, { rejectWithValue }) => {
   try {
     const apiKey = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
@@ -13,16 +13,16 @@ export const searchBooks = createAsyncThunk('search/searchBooks', async (searchB
 
     const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${searchBook}&key=${apiKey}`;
     console.log(apiUrl)
-    const response = await fetch(apiUrl);
+    const response = await fetch(apiUrl); //faz a chamada a api
 
-    if (!response.ok) {
+    if (!response.ok) { //se o pedido der erro
       throw new Error(`Failed to fetch data. Status: ${response.status}`);
     }
 
     const data = await response.json();
     return data.items || [];
   } catch (error) {
-    return rejectWithValue(error.message);
+    return rejectWithValue(error.message); // Se houver um erro, rejeita a ação c/ msg error
   }
 });
 
@@ -36,20 +36,20 @@ const searchSlice = createSlice({
     builder.addCase(searchBooks.pending, (state, action) => {
       const query = action.meta.arg;
       if (!query) {
-        // Consulta vazia, ignorar a atualização do estado
+        // se input da pesquisa estiver vazio, ignorar a atualização do estado
         return;
       }
 
-      state.status = 'loading';
+      state.status = 'loading'; //atualiza o estado da pesquisa para loading
     });
 
-    builder.addCase(searchBooks.fulfilled, (state, action) => {
+    builder.addCase(searchBooks.fulfilled, (state, action) => { // Atualiza o estado com os resultados da pesquisa c/ sucesso
       state.status = 'succeeded';
       state.searchResults = action.payload;
       state.error = null;
     });
 
-    builder.addCase(searchBooks.rejected, (state, action) => {
+    builder.addCase(searchBooks.rejected, (state, action) => { // Atualiza o estado com a mensagem de erro e indica o q falhou
       state.status = 'failed';
       state.error = action.payload;
     });
